d78731156293a962f06d972a415d5ff1a49e8379
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2023-05-23 17:29:18 -0700

    ew offset

--- preprocess.py
+++ preprocess.py
@@ -84,7 +84,7 @@ def preprocess_single_pass(path,outpath,lid_path):
         down_east_pcd = downsample_pcd(east_pcd)
         ew_pass_offset = 61.2552933403*(float(metadata['gantry_system_variable_metadata']['position z [m]'])) - 7.8968761675
 
-        merged_pcd,merged_down_pcd,new_east,new_east_down = merge_east_west_ransac(east_pcd,west_pcd,down_east_pcd,down_west_pcd,down_east_pcd, ew_pass_offset)
+        merged_pcd,merged_down_pcd,new_east,new_east_down = merge_east_west_ransac(east_pcd,west_pcd,down_east_pcd,down_west_pcd,ew_pass_offset)
 
         new_east = rotate_pcd(new_east,90,merged_down_pcd)
         new_west = rotate_pcd(west_pcd,90,merged_down_pcd)
